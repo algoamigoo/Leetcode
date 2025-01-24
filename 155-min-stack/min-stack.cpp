11bfbd1ec912a3e5d6e8e2de09eae223b25f0026
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-01-24 09:29:19 +0530

    Time: 3 ms (63.43%) | Memory: 23.1 MB (94.25%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,26 +1,31 @@
 class MinStack {
-  stack < pair < int, int >> st;
-
-  public:
-    void push(int x) {
-      int min;
-      if (st.empty()) {
-        min = x;
-      } else {
-        min = std::min(st.top().second, x);
-      }
-      st.push({x,min});
+public:
+    vector<pair<int, int>> arr;
+    pair<int, int> p;
+    int mini;
+    MinStack() {
+        p.first = 0;
+        p.second = 0;
+        mini = INT32_MAX;
     }
 
-  void pop() {
-    st.pop();
-  }
+    void push(int data) {
+        if (arr.empty()) {
+            p.first = data;
+            p.second = data;
+            arr.push_back(p);
+            mini = data;
+        } else {
+            p.first = data;
+            mini = min(data, arr.back().second);
+            p.second = mini;
+            arr.push_back(p);
+        }
+    }
 
-  int top() {
-    return st.top().first;
-  }
+    void pop() { arr.pop_back(); }
 
-  int getMin() {
-    return st.top().second;
-  }
+    int top() { return arr.back().first; }
+
+    int getMin() { return arr.back().second; }
 };
