cd4dfa796f4c9ac69a92b19937ab8cd18b7afd16
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-02-04 22:21:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.12%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,37 +1,16 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if (matrix.empty() || matrix[0].empty()) return false;
-        
+    bool searchMatrix(vector<vector<int> > &matrix, int target) {
         int n = matrix.size();
-        int n2 = matrix[0].size();
-
-        int l = 0, h = n - 1, row = -1;
-        while (l <= h) {
-            int m = (l + h) / 2;
-            if (matrix[m][0] > target)
-                h = m - 1;
-            else if (matrix[m][0] == target)
-                return true;
-            else {
-                row = m;
-                l = m + 1;
-            }
+        int m = matrix[0].size();
+        int l = 0, r = m * n - 1;
+        while (l != r){
+            int mid = (l + r - 1) >> 1;
+            if (matrix[mid / m][mid % m] < target)
+                l = mid + 1;
+            else 
+                r = mid;
         }
-
-        if (row == -1) return false;
-
-        l = 0, h = n2 - 1;
-        while (l <= h) {
-            int m = (l + h) / 2;
-            if (matrix[row][m] > target)
-                h = m - 1;
-            else if (matrix[row][m] == target)
-                return true;
-            else
-                l = m + 1;
-        }
-
-        return false;
+        return matrix[r / m][r % m] == target;
     }
 };
