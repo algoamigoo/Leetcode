e37d189aa3ec2cebc57c0ada377dd86fe1c6a47e
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-02-17 14:43:36 +0530

    Time: 33 ms (42.75%) | Memory: 16.8 MB (46.94%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -1,22 +1,32 @@
-#include <unordered_set>
-#include <string>
-#include <algorithm>
-
 class Solution {
 public:
+    int n;
+
+    void solve(string& tiles, vector<bool>& used, unordered_set<string>& result, string &curr) {
+        result.insert(curr);
+
+        for(int i = 0; i < n; i++) {
+            if(used[i])
+                continue;
+
+            curr.push_back(tiles[i]);
+            used[i] = true;
+
+            solve(tiles, used, result, curr);
+
+            used[i] = false;
+            curr.pop_back();
+        }
+    }
+
     int numTilePossibilities(string tiles) {
-        unordered_set<string> st;
-        int n = tiles.length();
-        
-        // Sort the tiles to handle duplicates
-        sort(tiles.begin(), tiles.end());
-        do {
-            for (int len = 1; len <= n; ++len) {
-                string s = tiles.substr(0, len);
-                st.insert(s);
-            }
-        } while (next_permutation(tiles.begin(), tiles.end()));
-        
-        return st.size();
+        n = tiles.length();
+        vector<bool> used(n, false);
+        unordered_set<string> result;
+        string curr = "";
+
+        solve(tiles, used, result, curr);
+
+        return result.size()-1;
     }
 };
