83b3a061d670c700993a0c61ba47f9ec3ac1ecef
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-02-19 12:17:38 +0530

    Time: 61 ms (29.48%) | Memory: 42 MB (27.63%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    void backtrack(set<vector<int>> &ans,int i, vector<int>&nums,vector<int> curr){
-            if(curr.size()==0 || curr.back()<=nums[i])
-            {curr.push_back(nums[i]);}
-
-            if(curr.size()>=2)
-            ans.insert(curr);
-
-            if(i+1<nums.size())
-            backtrack(ans,i+1,nums,curr);
-
-            if(curr.size()>=2)
-            {curr.pop_back();
-            if(i+1<nums.size())
-            backtrack(ans,i+1,nums,curr);}
-
-            return;
+    void backtrack(set<vector<int>> &ans, int i, vector<int> &nums, vector<int> &curr) {
+        if (curr.empty() || curr.back() <= nums[i]) {
+            curr.push_back(nums[i]);
+            if (curr.size() >= 2) ans.insert(curr);
+            if (i + 1 < nums.size()) backtrack(ans, i + 1, nums, curr);
+            curr.pop_back();
+        }
+        if (i + 1 < nums.size()) backtrack(ans, i + 1, nums, curr);
     }
-    vector<vector<int>> findSubsequences(vector<int>& nums) {
-        set<vector<int>> ans;
 
-        for(int i=0;i<nums.size()-1;i++){
+    vector<vector<int>> findSubsequences(vector<int> &nums) {
+        set<vector<int>> ans;
         vector<int> curr;
-        backtrack(ans,i,nums,curr);
+        for (int i = 0; i < nums.size() - 1; i++) {
+            backtrack(ans, i, nums, curr);
         }
-        vector<vector<int>> v;
-        for(auto it : ans){
-            v.push_back(it);
-        }
-        return v;
+        return vector<vector<int>>(ans.begin(), ans.end());
     }
 };
