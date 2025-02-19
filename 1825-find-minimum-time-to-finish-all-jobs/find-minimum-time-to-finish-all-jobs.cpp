0f1c16cec8c589718f184e865782debda20a37dd
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-02-19 14:59:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (43.52%) - LeetSync

--- 1825-find-minimum-time-to-finish-all-jobs/find-minimum-time-to-finish-all-jobs.cpp
+++ 1825-find-minimum-time-to-finish-all-jobs/find-minimum-time-to-finish-all-jobs.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
-    int n, ans;
+    int n;
     
-    void backtrack(int idx, vector<int>& cookies, vector<int>& children, int k) {
-        if (idx == n) {
-            ans = min(ans, *max_element(children.begin(), children.end()));
-            return;
-        }
+    bool canDistribute(vector<int>& cookies, vector<int>& children, int idx, int maxCookies) {
+        if (idx == n) return true;
         
-        for (int i = 0; i < k; i++) {  
-            children[i] += cookies[idx];
-            backtrack(idx + 1, cookies, children, k);
-            children[i] -= cookies[idx];
+        for (int i = 0; i < children.size(); i++) {
+            if (children[i] + cookies[idx] <= maxCookies) {
+                children[i] += cookies[idx];
+                if (canDistribute(cookies, children, idx + 1, maxCookies)) return true;
+                children[i] -= cookies[idx];
 
-            if (children[i] == 0) break;
+                if (children[i] == 0) break; // Prune duplicate states
+            }
         }
-    }
-    int minimumTimeRequired(vector<int>& cookies, int k) {
         
+        return false;
+    }
+
+    int minimumTimeRequired(vector<int>& cookies, int k) {
         n = cookies.size();
-        ans = INT_MAX;
-        vector<int> children(k, 0);
-        backtrack(0, cookies, children, k);
+        sort(cookies.rbegin(), cookies.rend());
+        int l = cookies[0], r = accumulate(cookies.begin(), cookies.end(), 0), ans = r;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            vector<int> children(k, 0);
+
+            if (canDistribute(cookies, children, 0, mid)) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        
         return ans;
     }
 };
