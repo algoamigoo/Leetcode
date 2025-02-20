557c2a1b49def39dfc8e15b37b3e6ebe35e08791
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-02-20 12:19:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (69.88%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,38 +1,11 @@
 class Solution {
 public:
-    std::string intToBinary(int num, int n) {
-        std::string binary = "";
-        while (num > 0) {
-            binary = (char)('0' + (num % 2)) + binary;
-            num /= 2;
-        }
-        while (binary.size() < n) {
-            binary = '0' + binary;
-        }
-        return binary;
-    }
-
     string findDifferentBinaryString(vector<string>& nums) {
         int n = nums.size();
-        vector<int> v;
-        
+        string ans = "";
         for (int i = 0; i < n; i++) {
-            int x = stoi(nums[i], nullptr, 2);
-            v.push_back(x);
+            ans += (nums[i][i] == '0') ? '1' : '0';
         }
-
-        sort(v.begin(), v.end());
-        int y = -1;
-
-        for (int i = 0; i < n; i++) {
-            if (v[i] != i) {
-                y = i;
-                break;
-            }
-        }
-
-        if (y == -1) y = n;
-
-        return intToBinary(y, n); 
+        return ans;
     }
 };
