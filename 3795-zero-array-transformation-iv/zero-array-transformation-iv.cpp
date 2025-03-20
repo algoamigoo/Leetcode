663da0ee15b1570abcf3bf2e665e63c5121220b1
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-03-20 20:16:22 +0530

    Time: 265 ms (22.53%) | Memory: 184.6 MB (16.91%) - LeetSync

--- 3795-zero-array-transformation-iv/zero-array-transformation-iv.cpp
+++ 3795-zero-array-transformation-iv/zero-array-transformation-iv.cpp
@@ -1,33 +1,30 @@
 class Solution {
 private:
-    int solve(vector<vector<int>>& q, int i, int t, int k, vector<vector<int>>& dp) {
+    int solve(int i, int target, int currq, vector<vector<int>>& q, vector<vector<int>>& dp) {
         // Base cases
-        if (t == 0) return k;
-        if (k >= q.size() || t < 0)      
+        if (target == 0) return currq;           
+        if (currq >= q.size() || target < 0)
             return q.size() + 1;
-        
-        // Memoized result
-        if (dp[t][k] != -1) return dp[t][k];
-        
-        // Skip current query
-        int res = solve(q, i, t, k + 1, dp);
-        
-        // Use query if index in range
-        if (q[k][0] <= i && i <= q[k][1])
-            res = min(res, solve(q, i, t - q[k][2], k + 1, dp));
-        
-        return dp[t][k] = res;
+
+        if (dp[target][currq] != -1) 
+        return dp[target][currq];
+
+        int res = solve(i, target, currq + 1, q, dp);
+
+        if (q[currq][0] <= i && i <= q[currq][1])
+            res = min(res, solve(i, target - q[currq][2], currq + 1, q, dp));
+
+        return dp[target][currq] = res;
     }
 
 public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
         int ans = -1;
-        
         for (int i = 0; i < nums.size(); ++i) {
-            vector<vector<int>> dp(nums[i] + 1,vector<int>(queries.size() + 1, -1));
-            ans = max(ans, solve(queries, i, nums[i], 0, dp));
+            vector<vector<int>> dp(nums[i] + 1, vector<int>(queries.size() + 1, -1));
+            ans = max(ans, solve(i, nums[i], 0, queries, dp));
         }
-        
+
         return (ans > queries.size()) ? -1 : ans;
     }
 };
