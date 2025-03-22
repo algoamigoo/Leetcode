97261c0455734ade67aeefc8799bc95f1814571d
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-03-22 15:54:59 +0530

    Time: 861 ms (24.51%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,6 +1,7 @@
-SELECT 
-    query_name, 
-    ROUND(AVG(rating / position), 2) AS quality, 
-    ROUND(100 * (SELECT COUNT(*) FROM Queries Q WHERE Q.query_name = Queries.query_name AND rating < 3) / COUNT(*), 2) AS poor_query_percentage
-FROM Queries
-GROUP BY query_name;
+SELECT  query_name,  
+    ROUND(AVG(rating / position), 2) AS quality,   
+    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / COUNT(*), 2) AS poor_query_percentage  
+FROM  
+    Queries  
+GROUP BY  
+    query_name;  
