70e8e57b09d75bade1e1db08fd2811911b83259c
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-03-22 14:37:56 +0530

    Time: 917 ms (83.35%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,3 @@
+SELECT Signups.user_id, COALESCE(ROUND(AVG(action='confirmed'),2),0) AS confirmation_rate
+FROM Signups LEFT JOIN Confirmations ON Signups.user_id=Confirmations.user_id
+GROUP BY Signups.user_id;
