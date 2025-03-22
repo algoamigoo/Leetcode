6ea6888f4177cb3667f446a26446335f6f8b5361
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-03-22 13:34:15 +0530

    Time: 775 ms (30.24%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,9 @@
-SELECT m.name
-FROM Employee AS e
-JOIN Employee AS m 
-WHERE m.id=e.managerID
-GROUP BY e.managerID
-HAVING COUNT(e.managerID)>=5;
+SELECT name
+FROM Employee
+WHERE id IN (
+    SELECT managerId
+    FROM Employee
+    WHERE managerId IS NOT NULL
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+);
