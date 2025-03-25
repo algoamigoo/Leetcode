9fd1ec8175a79fed237263fdfbabf66c876543a0
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-03-25 11:18:22 +0530

    Time: 355 ms (93.29%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,19 +1,7 @@
-WITH majdoor AS (
-    SELECT employee_id, manager_id
-    FROM Employees
-    WHERE salary < 30000
-),
-malik AS (
-    SELECT majdoor.employee_id, majdoor.manager_id
-    FROM majdoor
-    JOIN Employees ON Employees.employee_id = majdoor.manager_id
+SELECT employee_id
+FROM Employees
+WHERE salary < 30000
+AND manager_id NOT IN (
+  SELECT employee_id FROM Employees
 )
-SELECT majdoor.employee_id 
-FROM majdoor 
-WHERE majdoor.employee_id NOT IN (
-    SELECT employee_id 
-    FROM malik 
-    WHERE manager_id IS NOT NULL
-)
-AND majdoor.manager_id IS NOT NULL
-ORDER BY majdoor.employee_id;
+ORDER BY employee_id;
