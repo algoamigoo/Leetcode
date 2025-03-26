b22d137cc172c3115e3d7ead88a04f0b83f8796f
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-03-27 00:06:27 +0530

    Time: 52 ms (33.71%) | Memory: 84.5 MB (23.46%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
-    static bool comp(vector<int> &a, vector<int> &b){
-        if(a[0]==b[0]){
-            return a[1]>b[1];
+    static bool comp(vector<int> &a, vector<int> &b) {
+        if (a[0] == b[0]) {
+            return a[1] > b[1]; 
         }
-        return a[0]<b[0];
+        return a[0] < b[0]; 
     }
+
+    int lengthOfLIS(vector<int>& arr) {
+        vector<int> lis;
+        for (int x : arr) {
+            auto it = lower_bound(lis.begin(), lis.end(), x);
+            if (it == lis.end()) {
+                lis.push_back(x);
+            } else {
+                *it = x;
+            }
+        }
+        return lis.size();
+    }
+
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         sort(envelopes.begin(), envelopes.end(), comp);
-        int i,j,n=envelopes.size();
-        vector<int> lis;
-        for(int i=0;i<n;i++){
-            auto it = lower_bound(lis.begin(),lis.end(),envelopes[i][1]);
-            if(it==lis.end()){
-                lis.push_back(envelopes[i][1]);
-            }
-            else
-            *it=envelopes[i][1];
+        
+        vector<int> heights;
+        for (auto &env : envelopes) {
+            heights.push_back(env[1]);
         }
         
-    return lis.size();
+        return lengthOfLIS(heights);
     }
 };
