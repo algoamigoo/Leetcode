1a11dcc0fa89f4c3f20c05a8a7bac4058c5edb41
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-03-28 09:12:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (60.08%) - LeetSync

--- 827-expressive-words/expressive-words.cpp
+++ 827-expressive-words/expressive-words.cpp
@@ -1,53 +1,18 @@
 class Solution {
 public:
     int expressiveWords(string s, vector<string>& words) {
-        // problem statement
-        // we have to find the no of words form the given words which can
-        // be made stretchy in order to form given string
-
-        // condition is i have to extend so that size is 3 char or more for
-        // each letter that is stretched
-
-         int i, j, curr, count = 0;
-        for (int k = 0; k < words.size(); k++) {
-            i = 0;
-            j = 0;
-            curr = 1;
-            int f = 0;
-            
-            while (j < words[k].length() && i < s.length() && f == 0) {
-                curr = 1;
-                while (i + 1 < s.length() && s[i] == s[i + 1]) {
-                    curr++;
-                    i++;
-                }
-                
-                if (words[k][j] == s[i]) {
-                    int curr2 = 1;
-                    while (j + 1 < words[k].length() && words[k][j] == words[k][j + 1]) {
-                        curr2++;
-                        j++;
-                    }
-                    
-                    if (curr >= 3) {
-                        if (curr2 > curr)
-                            f = 1;
-                    } else {
-                        cout << words[k][j] << " " << curr2 << " " << curr << endl;
-                        if (curr != curr2)
-                            f = 1;
-                    }
-                } else {
-                    f = 1;
-                }
-                
-                i++;
-                j++;
+        int count = 0;
+        for (string w : words) {
+            int i = 0, j = 0;
+            bool valid = true;
+            while (i < s.length() && j < w.length() && valid) {
+                if (s[i] != w[j]) { valid = false; break; }
+                int sCount = 1; while (i + 1 < s.length() && s[i] == s[i + 1]) { sCount++; i++; }
+                int wCount = 1; while (j + 1 < w.length() && w[j] == w[j + 1]) { wCount++; j++; }
+                if (sCount < 3 && sCount != wCount || sCount >= 3 && wCount > sCount) valid = false;
+                i++; j++;
             }
-            
-            if (f == 0 && i == s.length() && j == words[k].length()) {
-                count++;
-            }
+            if (valid && i == s.length() && j == w.length()) count++;
         }
         return count;
     }
