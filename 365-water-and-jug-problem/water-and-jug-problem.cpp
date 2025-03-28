366383bd95f15023280422c7f2c957e1184ac57f
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-03-28 16:28:29 +0530

    Time: 2 ms (14.05%) | Memory: 8.7 MB (20.44%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/water-and-jug-problem.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
-   int GCD(int a, int b){
-    while(b != 0 ){
-        int temp = b;
-        b = a%b;
-        a = temp;
-    }
-    return a;
-   }
-    bool canMeasureWater(int a, int b, int t) {
-        int z = gcd(a,b);
-        if(a + b < t) return false;
-        if(t==a || t==b || t==a+b)
-        return true;
-        
-        if(t%z==0)
-        return true;
+    bool canMeasureWater(int a, int b, int target) {
+        /*
+        we will check the possible states
+        operations
+        1. pour form a to b
+        2. pour form b to a
+        3. empty a
+        4. empty b
+        5. fully fill a
+        6. fully fill b
+        */
+        set<pair<int,int>> used_states;
+        queue<pair<int,int>> q;
+        q.push({0,0});
+        int sum=0;
+
+        while(!q.empty()){
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            sum=x+y;
+            if(sum==target)
+            return true;
+            
+            vector<pair<int,int>> v;
+            v.push_back({0,x});
+            v.push_back({y,0});
+            v.push_back({max(0,a-x),min(b,y+x)});
+            v.push_back({min(a,x+y),max(0,b-y)});
+            v.push_back({a,y});
+            v.push_back({x,b});
+
+            for(auto it : v){
+                if(used_states.count(it)==0)
+                {
+                    q.push(it);
+                    used_states.insert(it);
+                }
+            }
+        }
         return false;
     }
 };
