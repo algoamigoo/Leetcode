b8be7154044897226f2c7b24c1850bcbb13d2e04
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-04-05 12:53:59 +0530

    Time: 4 ms (26.28%) | Memory: 9.2 MB (42.82%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int subsetXORSum(vector<int>& nums) {
-        return func(nums, 0, 0);
-    }
-    
-    int func(vector<int>& nums, int i, int currXor) {
-        if (i == nums.size()) return currXor;
-        return func(nums, i+1, currXor ^ nums[i]) + func(nums, i+1, currXor);
+        int n = nums.size();
+        int totalSubsets = 1 << n;
+        int ans = 0;
+        
+        for(int mask = 0; mask < totalSubsets; mask++) {
+            int subsetXOR = 0;
+            for(int i = 0; i < n; i++) {
+                if(mask & (1 << i)) { // If bit is set, include nums[i]
+                    subsetXOR ^= nums[i];
+                }
+            }
+            ans += subsetXOR;
+        }
+        return ans;
     }
 };
