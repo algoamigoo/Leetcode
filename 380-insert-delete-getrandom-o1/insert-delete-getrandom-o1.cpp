a6ad7be99ce193a15c6ab2604e21c438986718cd
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-04-17 00:50:38 +0530

    Time: 41 ms (75.34%) | Memory: 113.4 MB (9.33%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -1,42 +1,43 @@
 class RandomizedSet {
 public:
-    set<int> rset;
-    unordered_map<int,int> map;
+    vector<int> elements; // Stores the elements
+    unordered_map<int, int> map; // Maps value to its index in elements
     RandomizedSet() {
-
+        srand(time(0)); // Seed random number generator
     }
     
     bool insert(int val) {
-        if(map.count(val)==1)
-        return false;
+        if (map.count(val) && map[val] >= 0) // Value exists at a valid index
+            return false;
         
-        rset.insert(val);
-        map[val]++;
+        elements.push_back(val); // Add to vector
+        map[val] = elements.size() - 1; // Store index
         return true;
     }
     
     bool remove(int val) {
-        if(map.count(val)==0)
-        return false;
-        map[val]--;
-        if(map[val]==0)
-        {rset.erase(val);
-        map.erase(val);}
+        if (!map.count(val) || map[val] < 0) // Value doesn't exist or was removed
+            return false;
+        
+        int index = map[val]; // Get index of val
+        int lastVal = elements.back(); // Get last element
+        
+        // Move last element to index of val
+        elements[index] = lastVal;
+        map[lastVal] = index;
+        
+        // Remove val
+        elements.pop_back();
+        map[val] = -1; // Mark as removed (or erase: map.erase(val))
+        
         return true;
     }
     
     int getRandom() {
-        if(rset.size()==0)
-        return NULL;
-        int x = rand()%rset.size();
-        return *next(rset.begin(), x);
+        if (elements.empty())
+            throw runtime_error("NULL");
+        
+        int x = rand() % elements.size();
+        return elements[x];
     }
 };
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet* obj = new RandomizedSet();
- * bool param_1 = obj->insert(val);
- * bool param_2 = obj->remove(val);
- * int param_3 = obj->getRandom();
- */
