c21736bb4a129e6261147734f02bc70b8fff813d
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-05-11 16:21:24 +0530

    Time: 132 ms (52.22%) | Memory: 135.5 MB (50.51%) - LeetSync

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     long long minSum(vector<int>& nums1, vector<int>& nums2) {
-        long long sum1 = 0, sum2 = 0;
-        int z1 = 0, z2 = 0;
         
-        // Calculate sum and count zeros for nums1
-        for (int num : nums1) {
+        long long zeroCount1 = 0;
+        long long sum1 = 0;
+        long long zeroCount2 = 0;
+        long long sum2 = 0;
+
+        for(auto num : nums1) {
+            if(num == 0) {
+                zeroCount1++;
+            }
             sum1 += num;
-            if (num == 0) z1++;
         }
-        
-        // Calculate sum and count zeros for nums2
-        for (int num : nums2) {
+
+        for(auto num : nums2) {
+            if(num == 0) {
+                zeroCount2++;
+            }
             sum2 += num;
-            if (num == 0) z2++;
         }
-    
-        long long minSum1 = sum1 + z1;
-        long long minSum2 = sum2 + z2;
-        
-        if (z1 == 0 && z2 == 0) {
-            if (sum1 != sum2) return -1;
-            return sum1;
-        } else if (z1 == 0) {
-            if (sum1 < minSum2) return -1; 
-            return sum1;
-        } else if (z2 == 0) {
-            if (sum2 < minSum1) return -1; 
-            return sum2;
-        } else {
-            return max(minSum1, minSum2);
+
+        long long minSum1 = sum1 + zeroCount1;
+        long long maxSum1 = zeroCount1 > 0 ? LLONG_MAX : sum1;
+
+        long long minSum2 = sum2 + zeroCount2;
+        long long maxSum2 = zeroCount2 > 0 ? LLONG_MAX : sum2;
+
+        long long minSum = max(minSum1, minSum2);
+
+        if(minSum <= maxSum1 && minSum <= maxSum2) {
+            return minSum;
         }
+
+        return -1;
     }
 };
