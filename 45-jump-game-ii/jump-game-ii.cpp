d408e22ab419697ce285ee34dd1cc4ef9de54b2b
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-06-10 12:03:10 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (62.55%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int rec(int i, vector<int>& nums, int n, vector<int>& dp) {
-        if (i >= n - 1) return 0;
-        if (dp[i] != -1) return dp[i];
-
-        int ans = 1e9;
-        for (int j = 1; j <= nums[i]; j++) {
-            if (i + j < n) {
-                ans = min(ans, 1 + rec(i + j, nums, n, dp));
+    int jump(vector<int>& nums) {
+        int jumps = 0, farthest = 0, currEnd = 0;
+        int n = nums.size();
+        
+        for (int i = 0; i < n - 1; ++i) {
+            farthest = max(farthest, i + nums[i]);
+            
+            if (i == currEnd) {
+                jumps++;
+                currEnd = farthest;
             }
         }
-        return dp[i] = ans;
-    }
-
-    int jump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1);
-        return rec(0, nums, n, dp);
+        return jumps;
     }
 };
