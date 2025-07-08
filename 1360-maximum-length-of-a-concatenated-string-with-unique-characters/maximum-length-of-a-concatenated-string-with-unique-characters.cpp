dd92e08b9e8484b16317b06e9a25064d823d764e
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-07-08 17:20:28 +0530

    Time: 15 ms (74.67%) | Memory: 16.5 MB (71.94%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int n;
-
-    bool check(string s) {
-        int m[26] = {0}; // Initialize all to 0
-        for (char c : s) {
-            if (m[c - 'a']++) return false; // Already exists
+    bool check(string s){
+        int m[26]={0};
+        for(int i=0;i<s.length();i++){
+            if(m[s[i]-'a']!=0)
+            return false;
+            m[s[i]-'a']++;
         }
         return true;
     }
+    int func(int i,vector<string>& arr,string s){
+        if(i==n)
+        return s.size();
 
-    int func(int i, vector<string>& arr, string s) {
-        if (i == n) return s.size();
-
-        int nottake = func(i + 1, arr, s);
-
-        int take = 0;
-        if (check(s + arr[i])) {
-            take = func(i + 1, arr, s + arr[i]);
+        int nottake = func(i+1,arr,s);
+        int take=0;
+        if(check(s+arr[i])){
+            take = func(i+1,arr,s+arr[i]);
         }
-
-        return max(nottake, take);
+        return max(take,nottake);
     }
-
     int maxLength(vector<string>& arr) {
         n = arr.size();
-        return func(0, arr, "");
+        string s ="";
+        int ans = func(0,arr,s);
+        return ans;
     }
 };
