dd4b6a439bb19578a76922e1e5c2139bc8871c71
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-07-22 14:30:48 +0530

    Time: 9 ms (40.03%) | Memory: 33.1 MB (23.57%) - LeetSync

---
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,24 @@
+class KthLargest {
+public:
+
+    priority_queue<int,vector<int>,greater<int>> pq; // max heap, stores smallest at top
+    int queue_size=0;
+    KthLargest(int k, vector<int>& nums) {
+        queue_size=k;
+        for(int i=0;i<nums.size();i++)
+        pq.push(nums[i]);   
+
+    }
+    
+    int add(int val) {
+        pq.push(val);
+
+        if(pq.size()<=queue_size)
+        return pq.top();
+
+        while(pq.size()>queue_size)
+        pq.pop();
+
+        return pq.top();
+    }
+};
