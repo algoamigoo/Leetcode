29abc864b2c5e514b8cb3576723a4c5a5d0fb05d
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-07-22 14:53:46 +0530

    Time: 2 ms (36.53%) | Memory: 28.6 MB (5.24%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
-    vector<int> getFinalState(vector<int>& v, int k, int m) {
-        int n = v.size();
-        
-        for (int i = 0; i < k; ++i) {
-            int mn = *min_element(v.begin(), v.end());
-            
-            for (int& num : v) {
-                if (num == mn) {
-                    num *= m;
-                    break;
-                }
-            }
-        }
-        
-        return v;
+    static bool cmp(pair<int,int> a, pair<int,int> b){
+        return a.second<b.second;
+    }
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        int n = nums.size();
+         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq; // min heap
+         for(int i=0;i<nums.size();i++){
+            pq.push({nums[i],i});
+         }
+
+         while(k>0){
+            int x = pq.top().first;
+            int i = pq.top().second;
+            pq.pop();
+            pq.push({x*multiplier,i});
+            k--;
+         }
+         vector<pair<int,int>> ans;
+         while(!pq.empty())
+         {  
+            int x = pq.top().first;
+            int i = pq.top().second;
+            pq.pop();
+            ans.push_back({x,i});
+         }
+         vector<int> numbers;
+         sort(ans.begin(),ans.end(),cmp);
+         for(int i=0;i<n;i++){
+            numbers.push_back(ans[i].first);
+         }
+         return numbers;
     }
 };
