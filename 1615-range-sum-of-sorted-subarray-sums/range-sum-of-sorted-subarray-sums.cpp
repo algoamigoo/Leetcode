d80ae5097e1956417781d9fbb4197704213fa7e2
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-07-24 21:19:22 +0530

    Time: 63 ms (84.34%) | Memory: 11.1 MB (92.08%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,23 +1,38 @@
+// we can use heap if we want to find the subarray sum of an array in inc or decreasing order
+
 class Solution {
 public:
+    typedef pair<int, int> P;
+    int M = 1e9+7;
+
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        vector<int> subarrays;
-        const int MOD = 1e9 + 7;
+        priority_queue<P, vector<P>, greater<P>> pq;
         
+        //pair<sum, i> -> (subarray sum, till index i)
         for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += nums[j];
-                subarrays.push_back(sum);
+            pq.push({nums[i], i});
+        }
+
+        int result = 0;
+
+        for (int i = 1; i <= right; i++) {
+            auto p = pq.top();
+            pq.pop();
+
+            // If the current index is greater than or equal to left, add the
+            // value to the answer.
+            if (i >= left) {
+                result = (result + p.first) % M;
+            }
+
+            // If index is less than the last index, increment it and add its
+            // value
+            if (p.second < n - 1) {
+                p.second++;
+                p.first += nums[p.second];
+                pq.push(p);
             }
         }
-        sort(subarrays.begin(), subarrays.end());
-        
-        int result = 0;
-        for (int i = left - 1; i < right; i++) {
-            result = (result + subarrays[i]) % MOD;
-        }
-        
         return result;
     }
 };
