d353cec2199086dbc159db2d28deb038185a0e83
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-07-25 09:03:29 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (88.89%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
+    int n;
+     int backtrack(int i,vector<int> & nums,int sum){
+        if(i==n)
+        return sum;
+
+        return backtrack(i+1,nums,sum^nums[i])+backtrack(i+1,nums,sum);
+
+    }
     int subsetXORSum(vector<int>& nums) {
-        int n = nums.size();
-        int totalSubsets = 1 << n;
-        int ans = 0;
-        
-        for(int mask = 0; mask < totalSubsets; mask++) {
-            int subsetXOR = 0;
-            for(int i = 0; i < n; i++) {
-                if(mask & (1 << i)) { // If bit is set, include nums[i]
-                    subsetXOR ^= nums[i];
-                }
-            }
-            ans += subsetXOR;
-        }
-        return ans;
+        // xor twice to cancel xor use backtracking
+        n=nums.size();
+        int sum=0;
+        return backtrack(0,nums,0);
+
     }
 };
