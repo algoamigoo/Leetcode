18a1b4d5af17c28ffb1af0f0dbe4a991093c25dd
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-07-25 11:07:06 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (30.80%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void backtrack(vector<int>& nums, vector<int>& curr, vector<bool>& used) {
+        if (curr.size() == nums.size()) {
+            ans.push_back(curr);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (used[i]) continue;
+
+            used[i] = true;
+            curr.push_back(nums[i]);
+            backtrack(nums, curr, used);
+            curr.pop_back();
+            used[i] = false;
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> curr;
+        vector<bool> used(n, false);
+        backtrack(nums, curr, used);
+        return ans;
+    }
+};
