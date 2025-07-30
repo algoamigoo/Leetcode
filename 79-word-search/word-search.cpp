b8d78fc351e9a96f3b9e3dc85372b8d3d01bfca7
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-07-30 14:01:55 +0530

    Time: 728 ms (40.66%) | Memory: 11.7 MB (35.62%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,41 +1,46 @@
 class Solution {
 public:
-    int n, m;
-    vector<vector<int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int n,m;
+    vector<int> dx = {0,0,-1,1};
+    vector<int> dy = {1,-1,0,0};
 
-    bool isvalid(int i, int j) {
-        return i >= 0 && j >= 0 && i < n && j < m;
-    }
+    bool backtrack(int idx,int i,int j, vector<vector<char>>& board,string word){
 
-    bool backtrack(int i, int j, int index, string& word, vector<vector<char>>& board) {
-        if (index == word.size()) 
+        if(idx==word.size())
         return true;
 
-        if (!isvalid(i, j) || board[i][j] != word[index]) return false;
+        if(board[i][j]!=word[idx])
+        return false;
 
         char temp = board[i][j];
         board[i][j] = '#';
 
-        for (auto& d : dirs) {
-            int ni = i + d[0], nj = j + d[1];
-            if (backtrack(ni, nj, index + 1, word, board))
-                return true;
-        }
-
-        board[i][j] = temp;
-        return false;
-    }
-
-    bool exist(vector<vector<char>>& board, string word) {
-        n = board.size();
-        m = board[0].size();
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (backtrack(i, j, 0, word, board))
-                    return true;
+        for(int k=0;k<4;k++){
+            int ni = i+dx[k];
+            int nj = j+dy[k];
+            if((ni<n && nj<m && ni>=0 && nj>=0) &&
+            backtrack(idx+1,ni,nj,board,word)){
+            return true;
             }
         }
+        board[i][j]=temp;
+        return false;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        if(board[0][0]==word[0] && word.size()==1)
+        return true;
+           n = board.size();
+           m = board[0].size();
+
+            if(word.size())
+
+           for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(backtrack(0,i,j,board,word))
+                return true;
+            }
+           }
+
         return false;
     }
 };
