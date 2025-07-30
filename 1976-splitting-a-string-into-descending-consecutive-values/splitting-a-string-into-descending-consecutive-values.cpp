fcf3de71b7a08e229a7a294d0ada25d1ec4ee489
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-07-30 19:26:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (80.58%) - LeetSync

---
+++ 1976-splitting-a-string-into-descending-consecutive-values/splitting-a-string-into-descending-consecutive-values.cpp
@@ -0,0 +1,22 @@
+long long MX = 99999999999;
+class Solution {
+public:
+    bool dfs(string &s, long long prev, int idx, int cnt) {
+        if(idx == s.size() ) return cnt > 1;
+        long long num = 0;
+        for(int i = idx; i < s.size(); i++) {
+            num = num *  10l + s[i] - '0';
+            if(num > MX) break;
+            if(num == prev - 1 || prev == -1) {
+                if(dfs(s, num, i + 1, cnt + 1)) return true;
+            }
+            if(num > prev && prev != -1) break;
+        }
+        return false;
+    }
+    
+    bool splitString(string s) {
+        if(s.size() <= 1) return false;
+        return dfs(s, -1, 0, 0);
+    }
+};
