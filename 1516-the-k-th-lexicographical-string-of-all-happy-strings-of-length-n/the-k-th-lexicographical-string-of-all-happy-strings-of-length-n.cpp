c39a009db4ae6ca9f9154854337b2d05f4d4372d
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-07-31 13:45:14 +0530

    Time: 6 ms (59.12%) | Memory: 11.3 MB (55.56%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -2,25 +2,29 @@ class Solution {
 public:
     vector<string> ans;
 
-    void backtrack(int n, int k, string curr) {
+    void backtrack(int idx, int n, int k, vector<char> &c, string curr, char prev) {
         if (curr.size() == n) {
             ans.push_back(curr);
             return;
         }
 
-        if (ans.size() >= k) return;
+        if (ans.size() >= k)
+            return;
 
-        for (char ch : {'a', 'b', 'c'}) {
-            if (curr.empty() || curr.back() != ch) {
-                backtrack(n, k, curr + ch);
+        for (int i = 0; i < 3; i++) {
+            if (c[i] != prev) {
+                backtrack(idx + 1, n, k, c, curr + c[i], c[i]);
             }
         }
     }
 
     string getHappyString(int n, int k) {
-        backtrack(n, k, "");
+        vector<char> c = {'a', 'b', 'c'};
+        backtrack(0, n, k, c, "", '/');  // '/' is a dummy char not in {'a','b','c'}
+        
         if (k <= ans.size())
             return ans[k - 1];
+
         return "";
     }
 };
