e701e19ca7694f05aeeecbbfa0bc363d4de91d76
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-08-15 19:38:11 +0530

    Time: 435 ms (21.45%) | Memory: 262.5 MB (28.20%) - LeetSync

--- 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.cpp
+++ 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.cpp
@@ -1,34 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int rows, cols;
     vector<vector<int>> dirs = {{1,0},{-1,0},{0,1},{0,-1}};
 
-    bool dfs(int r, int c, vector<vector<int>>& grid, vector<vector<bool>>& vis) {
-        if (r == rows - 1) return true; // reached bottom row
-        vis[r][c] = true;
-
-        for (auto& d : dirs) {
-            int nr = r + d[0], nc = c + d[1];
-            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols &&
-                !vis[nr][nc] && grid[nr][nc] == 0) {
-                if (dfs(nr, nc, grid, vis)) return true;
-            }
-        }
-        return false;
-    }
-
-    bool func(int day, vector<vector<int>>& cells) {
+    bool canCross(int day, vector<vector<int>>& cells) {
         vector<vector<int>> grid(rows, vector<int>(cols, 0));
-        // Mark water for first `day` days
+
+        // Flood the grid for the first `day` days
         for (int i = 0; i <= day; i++) {
             grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
         }
+
+        queue<pair<int,int>> q;
         vector<vector<bool>> vis(rows, vector<bool>(cols, false));
 
-        // Try starting from any land cell in top row
+        // Start BFS from all possible cells in top row
         for (int c = 0; c < cols; c++) {
-            if (grid[0][c] == 0 && !vis[0][c]) {
-                if (dfs(0, c, grid, vis)) return true;
+            if (grid[0][c] == 0) {
+                q.push({0, c});
+                vis[0][c] = true;
+            }
+        }
+
+        while (!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+
+            if (r == rows - 1) return true; // reached bottom
+
+            for (auto& d : dirs) {
+                int nr = r + d[0], nc = c + d[1];
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols &&
+                    !vis[nr][nc] && grid[nr][nc] == 0) {
+                    vis[nr][nc] = true;
+                    q.push({nr, nc});
+                }
             }
         }
         return false;
@@ -38,19 +47,16 @@ public:
         rows = row;
         cols = col;
 
-        int l = 0, r = row * col - 1;
-        int ans = 0;
-
+        int l = 0, r = row * col - 1, ans = 0;
         while (l <= r) {
             int mid = l + (r - l) / 2;
-            if (func(mid, cells)) {
+            if (canCross(mid, cells)) {
                 ans = mid;
                 l = mid + 1;
             } else {
                 r = mid - 1;
             }
         }
-
-        return ans + 1; // +1 because days are 1-indexed in problem
+        return ans + 1; // days are 1-indexed
     }
 };
