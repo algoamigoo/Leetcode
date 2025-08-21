72ee8068c6deb93eb60f51c5c2483fa5c8ea8c92
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-08-21 21:49:49 +0530

    Time: 21 ms (95.21%) | Memory: 39.2 MB (80.18%) - LeetSync

---
+++ 721-accounts-merge/accounts-merge.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    vector<int> rank, parent;
+
+    int find(int x) {
+        if (x == parent[x])
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    void Union(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if (rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if (rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        int n = accounts.size();
+        parent.resize(n);
+        rank.resize(n, 0);
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+
+        unordered_map<string, int> emailToAcc;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < accounts[i].size(); j++) {
+                string email = accounts[i][j];
+                if (emailToAcc.count(email)) {
+                    Union(i, emailToAcc[email]);
+                } else {
+                    emailToAcc[email] = i;
+                }
+            }
+        }
+
+        unordered_map<int, vector<string>> merged;
+        for (auto& p : emailToAcc) {
+            string email = p.first;
+            int root = find(p.second);
+            merged[root].push_back(email);
+        }
+
+        vector<vector<string>> ans;
+        for (auto &it : merged) {
+            int root = it.first;
+            vector<string> emails = it.second;
+            sort(emails.begin(), emails.end()); // optional
+            vector<string> mergedAcc;
+            mergedAcc.push_back(accounts[root][0]); // name
+            mergedAcc.insert(mergedAcc.end(), emails.begin(), emails.end());
+            ans.push_back(mergedAcc);
+        }
+
+        return ans;
+    }
+};
