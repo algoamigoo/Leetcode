f6e33431a670dc6354e982ed775491755f900462
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-08-22 19:16:57 +0530

    Time: 14 ms (76.03%) | Memory: 25.6 MB (38.90%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.cpp
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        vector<vector<int>> dis(n,vector<int>(m,-1));
-        // dont need visited, just see if dis set to -1 or not
-
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> dis(n, vector<int>(m, -1));
         queue<pair<int,int>> q;
-        // queue all the land cell coordinates
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
@@ -17,43 +13,25 @@ public:
                 }
             }
         }
-        // now i will traverse from all the queued nodes until my queue is empty
-        // make a new grid storing the distances from the land nodes
-        int dx[4]={0,0,1,-1};
-        int dy[4]={1,-1,0,0};
-        int s = 1;
+
+        if (q.empty() || q.size() == n*m) return -1;
+
+        int dx[4]={0,0,1,-1}, dy[4]={1,-1,0,0};
+        int ans = -1;
+
         while(!q.empty()){
-            int sz = q.size();
-            while(sz--){
-                auto [x,y]=q.front();
-                q.pop();
-                for(int k=0;k<4;k++){
-                    int nx = x+dx[k];
-                    int ny = y+dy[k];
-                    
-                    if(nx>=0 && ny>=0 && nx<n && ny<m && dis[nx][ny]==-1){
-                        dis[nx][ny]=s;
-                        q.push({nx,ny});
-                    }
-                }
-            }
-            s++;
-        }
+            auto [x,y]=q.front();
+            q.pop();
 
-
-        // at last iterate through all the nodes
-        // check for grid[i][j]==0 and track the minimum and that my answer
-        int ans= INT_MIN;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==0){
-                    ans=max(ans,dis[i][j]);
+            for(int k=0;k<4;k++){
+                int nx = x+dx[k], ny = y+dy[k];
+                if(nx>=0 && ny>=0 && nx<n && ny<m && dis[nx][ny]==-1){
+                    dis[nx][ny] = dis[x][y] + 1;
+                    ans = max(ans, dis[nx][ny]);
+                    q.push({nx,ny});
                 }
             }
         }
-        if(ans==INT_MIN)
-        return -1;
-        
         return ans;
     }
 };
