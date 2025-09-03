29205b08c566ccb3f21eff75412906e9a98af9e7
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-09-03 20:05:58 +0530

    Time: 4 ms (25.94%) | Memory: 11.5 MB (50.51%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        unordered_set<string> st;
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.size(); j++) {
+                if (i != j && words[j].find(words[i]) != string::npos) {
+                    st.insert(words[i]);
+                }
+            }
+        }
+        return vector<string>(st.begin(), st.end()); 
+    }
+};
