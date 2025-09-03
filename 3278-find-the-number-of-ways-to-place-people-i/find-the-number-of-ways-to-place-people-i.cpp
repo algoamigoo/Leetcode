273eadbf9aa0d54762d267f0f0b56f8c762e3577
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-09-03 19:27:53 +0530

    Time: 1 ms (85.51%) | Memory: 32.6 MB (72.46%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -7,21 +7,25 @@ public:
     }
 
     int numberOfPairs(vector<vector<int>>& v) {
+
         int n = v.size();
+
         sort(v.begin(), v.end(), cmp);
+
         int ans = 0;
-        
          for(int i=0;i<n;i++) {
+
             int x1=v[i][0],y1=v[i][1];
-            long long k=-10000000000;
+            long long currmn=-1;
+
             for(int j=i+1;j<n;j++) {
                 int x2=v[j][0],y2=v[j][1];
                 if(y2>y1)
                     continue;
-                if(y2>k)
+                if(y2>currmn)
                     ans++;
-                if(y2>k)
-                    k=y2;
+                if(y2>currmn)
+                    currmn=y2;
             }
         }
         return ans;
