3ae7e156553b7273d9b8d96a14d91eb62d986542
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-09-03 19:48:27 +0530

    Time: 0 ms (100.00%) | Memory: 76.7 MB (7.99%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        int n = arr.size();
-        int i = 0, j = n - 1;
-        int mx = 0;
-        for (int i = 0; i < n; i++) {
-
-            while (i < j) {
-                mx = max(mx, arr[j]);
-                j--;
-            }
-            arr[i]=mx;
-            mx=0;
-            j=n-1;
+        int mx=0,n=arr.size();
+        vector<int> ans;
+        ans.push_back(-1);
+        for(int i=n-1;i>=1;i--){
+            mx=max(mx,arr[i]);
+            ans.push_back(mx);
         }
-        arr[n-1]=-1;
-        return arr;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
