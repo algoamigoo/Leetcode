000e6a6d98acb67eda27daee4d23ff1911ff89a1
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-09-29 18:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (44.38%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
@@ -1,22 +1,14 @@
 class Solution {
-    vector<vector<int>>dp;
-    int f(int i,int j,vector<int>&v){
-        if(j-i+1 == 3){
-            return (v[i]*v[i+1]*v[j]);
+public: int minScoreTriangulation(vector<int>& A) {
+        int n = A.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+        for (int j = 2; j < n; ++j) {
+            for (int i = j - 2; i >= 0; --i) {
+                dp[i][j] = INT_MAX;
+                for (int k = i + 1; k < j; ++k)
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j] + A[i] * A[j] * A[k]);
+            }
         }
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ans = INT_MAX;
-        for(int k=i+1; k<j; k++){
-            int temp = (v[i]*v[k]*v[j]) + f(i,k,v) + f(k,j,v);
-            ans = min(ans,temp);
-        }
-        return dp[i][j] = (ans==INT_MAX?0:ans);
-    }
-public:
-    int minScoreTriangulation(vector<int>& v) {
-        int n = v.size();
-        dp.resize(n+1,vector<int>(n+1,-1));
-        int ans = f(0,n-1,v);
-        return ans;
+        return dp[0][n - 1];
     }
 };
