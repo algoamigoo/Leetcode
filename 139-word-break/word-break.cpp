17b4718b62ff9716472da893e60e35d59ba5cc32
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-10-04 11:21:16 +0530

    Time: 29 ms (8.84%) | Memory: 22.5 MB (8.83%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> st(wordDict.begin(), wordDict.end());
-        int n = s.size();
-        vector<bool> dp(n + 1, false);
-        dp[n] = true; // empty suffix is valid
-
-        int maxLen = 0;
-        for (auto &w : wordDict) maxLen = max(maxLen, (int)w.size()); // optimization: no need to check longer words
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int len = 1; len <= maxLen && i + len <= n; len++) {
-                if (dp[i + len] && st.find(s.substr(i, len)) != st.end()) {
-                    dp[i] = true;
-                    break;
-                }
-            }
+    unordered_set<string> st;
+    int t[301];
+    int n;
+    
+    bool solve(string &s, int idx) {
+        
+        if(idx == n) {
+            return true;
         }
-        return dp[0];
+
+        if(st.find(s.substr( idx , n - idx )) != st.end()) {
+            return true;
+        }
+        
+        if(t[idx] != -1)
+            return t[idx];
+        
+        for(int l = 1; l<=n; l++) {
+            
+            string temp = s.substr(idx, l);
+            if(st.find(temp) != st.end() && solve(s, idx+l))
+                return t[idx] = true;
+            
+        }
+        
+        return t[idx] = false;
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        n = s.length();
+        
+        memset(t, -1, sizeof(t));
+        
+        for(string &word : wordDict) {
+            st.insert(word);
+        }
+        
+        return solve(s, 0);
     }
 };
