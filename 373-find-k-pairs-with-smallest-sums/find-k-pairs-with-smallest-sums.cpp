46a1852c98099e34e880f6af384ef293913bed6f
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-10-08 22:40:00 +0530

    Time: 121 ms (14.13%) | Memory: 131.8 MB (97.52%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -1,34 +1,58 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size(), m = nums2.size();
         vector<vector<int>> ans;
-        int n1 = nums1.size(), n2 = nums2.size();
-        //first find sum for kth smallest
-        long long lo = nums1[0]+nums2[0], hi = nums1[n1-1]+nums2[n2-1];
-        while(lo < hi){
-            long long mid = (lo+hi)>>1;
-            long long ct = 0,j = 0;
-            for(int i = 0, j = n2 - 1; i < n1; ++i) {
-                while(j >= 0 && nums1[i] + nums2[j] > mid) j--;
-                ct += j + 1;
+        if (n == 0 || m == 0 || k == 0) return ans;
+
+        // Binary search over possible sums
+        int low = nums1[0] + nums2[0];
+        int high = nums1.back() + nums2.back();
+
+        // Helper: count pairs with sum <= mid
+        auto countPairs = [&](int mid) {
+            long long cnt = 0;
+            int j = m - 1;
+            for (int i = 0; i < n; i++) {
+                // move j while sum > mid
+                while (j >= 0 && nums1[i] + nums2[j] > mid)
+                    j--;
+                cnt += (j + 1);
             }
-            if(ct < k) lo = mid+1;
-            else hi = mid;
+            return cnt;
+        };
+
+        // Binary search for smallest sum threshold
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (countPairs(mid) >= k)
+                high = mid;
+            else
+                low = mid + 1;
         }
-        for(long long i=0;i<n1;i++){
-            for(long long j=0;(j<n2) && (nums1[i]+nums2[j] < lo);j++){
-                ans.push_back({nums1[i],nums2[j]});
+        int targetSum = low;
+
+        // Collect all pairs with sum < targetSum
+        int j = m - 1;
+        for (int i = 0; i < n; i++) {
+            while (j >= 0 && nums1[i] + nums2[j] >= targetSum)
+                j--;
+            for (int t = 0; t <= j; t++) {
+                ans.push_back({nums1[i], nums2[t]});
+                if ((int)ans.size() == k) return ans;
             }
         }
-        unordered_map<int,int> mp;
-        for(auto it:nums2) mp[it]++;
-        for(int i=0;i<n1;i++){
-            int sz=mp[lo-nums1[i]];
-            while(sz>0 && ans.size()<k){
-                sz--;
-                ans.push_back({nums1[i],(int)(lo-nums1[i])});
+
+        // Collect pairs with sum == targetSum
+        for (int i = 0; i < n; i++) {
+            for (int t = 0; t < m; t++) {
+                if (nums1[i] + nums2[t] == targetSum) {
+                    ans.push_back({nums1[i], nums2[t]});
+                    if ((int)ans.size() == k) return ans;
+                }
             }
         }
+
         return ans;
     }
 };
